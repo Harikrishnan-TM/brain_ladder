ff13d63da896c5fd3540524a72b33575adafcd2b
Harikrishnan-TM <techdatamaxonline@gmail.com>
2025-08-19 09:54:03 -0700

    pushing

--- brain_ladder/settings.py
+++ brain_ladder/settings.py
@@ -16,7 +16,7 @@ DEBUG = config("DEBUG", default=False, cast=bool)
 ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="", cast=Csv())
 
 # =========================
-# Installed apps
+# Installed apps ok ok
 # =========================
 INSTALLED_APPS = [
     "django.contrib.admin",
